2d9c88a932266f007612193733517c8cbacec536
StyledStrike <lameulucas@gmail.com>
2024-07-28 10:10:24 -0300

    Don't draw halos for dead/dormant players

--- lua/squad_menu/client/hud.lua
+++ lua/squad_menu/client/hud.lua
@@ -211,7 +211,7 @@ SquadMenu.DrawHalos = function()
     for _, member in ipairs( squad.members ) do
         local ply = byId[member.id]
 
-        if ply and ply ~= me then
+        if ply and ply ~= me and ply:Alive() and not ply:IsDormant() then
             dist = origin:DistToSqr( ply:EyePos() )
 
             if dist < haloDistance then
